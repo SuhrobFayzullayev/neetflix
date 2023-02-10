4d3733e5df2eb23d57ca2d8fe3674f5448235ef4
Пользователь <suhrob.coder0604@gmail.com>
2023-02-10 09:53:07 +0300

    tgertgdgw

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+const withCSS = require("@zeit/next-css");
+
+module.exports = withCSS({
+  cssLoaderOptions: {
+    url: false,
+  },
+});
+
 const nextConfig = {
   experimental: {
     appDir: true,
